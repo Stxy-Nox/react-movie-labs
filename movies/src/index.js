41c21064be6baef9593d63ce6244803a04b7b995
SytxNox <seven_night_w@hotmail.com>
2024-11-04 01:42:17 +0000

    Added Upcoming movies page.

--- movies/src/index.js
+++ movies/src/index.js
@@ -10,6 +10,7 @@ import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
+import UpcomingPage from './pages/upcomingMoviesPage.js'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,6 +33,7 @@ const App = () => {
             <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
+            <Route path="/movies/upcoming" element={<UpcomingPage/>}/>
             <Route path="*" element={ <Navigate to="/" /> } />
             <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
           </Routes>
---
+++ movies/src/pages/upcomingMoviesPage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+
+const UpcomingPage = (props) => {
+    const {data,error, isLoading, isError} = useQuery('upcoming',getUpcomingMovies)
+
+    if (isLoading) {
+        return  <Spinner/>
+    }
+
+    if (isError) {
+        return <h1>{error.message}</h1>
+    }
+    const movies = data.results;
+
+    const favorites = movies.filter(m => m.favorite)
+    localStorage.setItem('favourites',JSON.stringify(favorites))
+    const addToFavorites = (movieId) => true    
+
+    return (
+        <PageTemplate
+          title="Discover Movies"
+          movies={movies}
+          action={(movie) => {
+            return <AddToFavoritesIcon movie={movie} />
+          }}
+        />
+    );
+}
+export default UpcomingPage;
