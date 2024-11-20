f664d76bf6027a89f5d63d2ab2eb9121e4c52172
SytxNox <seven_night_w@hotmail.com>
2024-11-20 23:24:24 +0000

    add trending page and router it

--- movies/src/index.js
+++ movies/src/index.js
@@ -12,6 +12,7 @@ import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
 import UpcomingPage from './pages/upcomingMoviesPage.js'
 import PlaylistPage from "./pages/playlistPage.js";
+import TrendingPage from "./pages/trendingPage.js";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -35,6 +36,7 @@ const App = () => {
             <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
+            <Route path="/movies/trending/today" element={<TrendingPage/>} />
             <Route path="/movies/upcoming" element={<UpcomingPage/>}/>
             <Route path="*" element={ <Navigate to="/" /> } />
             <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
