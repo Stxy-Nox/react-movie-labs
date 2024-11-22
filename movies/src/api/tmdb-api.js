17e13475771fa1b4ef1abc5b0b58b058085dab14
SytxNox <seven_night_w@hotmail.com>
2024-11-22 22:18:55 +0000

    add  parameter to getmovies api

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -1,6 +1,8 @@
-export const getMovies = () => {
+export const getMovies = (args) => {
+  const [,page] = args.queryKey;
+  console.log("current page: "+page)
   return fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${page}`
   ).then((response) => {
     if (!response.ok) {
       return response.json().then((error) => {
@@ -34,7 +36,7 @@ export const getTrendings = (args) => {
   
   const [,timeWindow] = args.queryKey;
   console.log(timeWindow);
-  
+
   return fetch(
     `https://api.themoviedb.org/3/trending/movie/${timeWindow}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
   ).then((response) => {
