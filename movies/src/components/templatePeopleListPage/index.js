58e4168f855e9ecfa1a95d4a29ff48d35dc9ee31
SytxNox <seven_night_w@hotmail.com>
2024-11-23 23:30:26 +0000

    finish people list page template

--- movies/src/components/templatePeopleListPage/index.js
+++ movies/src/components/templatePeopleListPage/index.js
@@ -2,3 +2,18 @@ import React, { useState } from "react";
 import Header from "../headerMovieList";
 import MovieList from "../movieList";
 import Grid from "@mui/material/Grid2";
+
+function PeopleListPageTemplate({ people, title }) {
+
+    return (
+      <Grid container>
+        <Grid size={12}>
+          <Header title={title} />
+        </Grid>
+        <Grid container sx={{flex: "1 1 500px"}}>
+          <MovieList  people={people}></MovieList>
+        </Grid>
+      </Grid>
+    );
+  }
+  export default PeopleListPageTemplate;
