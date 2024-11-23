23265124de8be8ddd99f704e7201f0c92b4f2131
SytxNox <seven_night_w@hotmail.com>
2024-11-23 23:20:55 +0000

    remove useless code in popular page

---
+++ movies/src/pages/popularPeoplePage.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import {  getPopular } from "../api/tmdb-api";
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import BasicPagination from "../components/pagination";
+
+const PopularPeoplePage = (props) => {
+  const [currentPage, setCurrentPage] = useState(1);//dont need to cached
+  const {  data, error, isLoading, isError }  = useQuery(['popular',currentPage], getPopular)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+
+  const totalPages = data.total_pages;
+  // console.log("total pages: "+totalPages)
+
+
+  return (
+    
+    <BasicPagination 
+    currentPage={currentPage}
+    totalPages={totalPages}
+    onPageChange={(page) => setCurrentPage(page)}
+  />
+);
+}
+export default PopularPeoplePage;
