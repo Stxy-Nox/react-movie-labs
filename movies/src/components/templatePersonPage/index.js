7d4adf959cafbecb2b0b593bcba3a1fb8ceb1964
SytxNox <seven_night_w@hotmail.com>
2024-11-24 19:16:58 +0000

    add return context

--- movies/src/components/templatePersonPage/index.js
+++ movies/src/components/templatePersonPage/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import Grid from "@mui/material/Grid2";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
-import { getMovieImages } from "../../api/tmdb-api";
+import { getPersonImages } from "../../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from '../spinner'
+
 
 const TemplatePersonPage = ({ person, children}) => {
 
@@ -13,6 +14,49 @@ const TemplatePersonPage = ({ person, children}) => {
     getPersonImages
   );
 
+  if (isLoading) {
+    return <Spinner/>
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }
+  const images = data.profiles
+
+  return (
+    <>
+    
+    <Grid container spacing={5} style={{ Padding:"15px"}}>
+      <Grid size={{xs:3}}>
+      <div sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+      }}>
+        <ImageList
+          sx={{
+            height:"100vh",
+          }} 
+          cols={1}
+        >
+          {images.map((image) => (
+            <ImageListItem key={images.file_path} cols={1}>
+              <img 
+                src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+                alt={image.profile_path}
+            />
+            </ImageListItem>
+          ))}
+        </ImageList> 
+      </div>
+      </Grid>
+
+      <Grid size={{xs:9}}>
+        {children}
+      </Grid>
+    </Grid>
+    </>
+  )
 }
 
 export default TemplatePersonPage;
