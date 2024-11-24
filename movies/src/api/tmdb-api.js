6df693ed27d6ecf3e91aa4fdf79eedbabeb63d1e
SytxNox <seven_night_w@hotmail.com>
2024-11-24 17:53:01 +0000

    add get person images api

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -160,6 +160,23 @@ export const getPreson = (args) => {
    });
   };
 
+  export const getPersonImages = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+      `https://api.themoviedb.org/3/person/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then( (response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
 
 
   export const getMovieReviews = ({ queryKey }) => {
