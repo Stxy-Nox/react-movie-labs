b18e4b59d37c378001343d2154ce4ca05b9a1d90
SytxNox <seven_night_w@hotmail.com>
2024-11-24 15:44:24 +0000

    add getperson api

--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -4,7 +4,7 @@ import React from "react";
 import { useParams } from 'react-router-dom';
 import MovieDetails from "../components/movieDetails/";
 import PageTemplate from "../components/templateMoviePage";
-// import useMovie from "../hooks/useMovie";
+// import  from "../hooks/useMovie";
 import { getMovie } from '../api/tmdb-api'
 import { useQuery } from "react-query";
 import Spinner from '../components/spinner'
