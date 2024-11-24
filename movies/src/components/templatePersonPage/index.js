39663ba31aa04f5e2cb06c4e6a6277afe062f669
SytxNox <seven_night_w@hotmail.com>
2024-11-24 17:50:09 +0000

    new template person page

---
+++ movies/src/components/templatePersonPage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Grid from "@mui/material/Grid2";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { getMovieImages } from "../../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from '../spinner'
+
+const TemplatePersonPage = ({ person, children}) => {
+
+
+}
+
+export default TemplatePersonPage;
