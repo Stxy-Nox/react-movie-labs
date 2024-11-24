2bece142c22c75a00f4a2676522ff3324d1b3e22
SytxNox <seven_night_w@hotmail.com>
2024-11-24 20:36:10 +0000

    import mui Standard image list

--- movies/src/components/KnowForList/index.js
+++ movies/src/components/KnowForList/index.js
@@ -14,7 +14,18 @@ const KnownForList = ({ knownFor }) => {
       <Typography variant="h6" component={"h4"} gutterBottom>
         Known for :
       </Typography>
-      
+      <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </div>
   )
 }
