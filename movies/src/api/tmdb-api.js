e97b0cd95cfe94a40f395a93bca85a304ec06f6f
SytxNox <seven_night_w@hotmail.com>
2024-11-24 20:20:54 +0000

    add get person's credit movie api

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -127,6 +127,25 @@ export const getPreson = (args) => {
     throw error
  });
 };
+
+export const getPresonCredits = (args) => {
+  //console.log(args)
+  const [, idPart] = args.queryKey;
+  const { id } = idPart;
+  return fetch(
+    `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  ).then((response) => {
+    if (!response.ok) {
+      return response.json().then((error) => {
+        throw new Error(error.status_message || "Something went wrong");
+      });
+    }
+    return response.json();
+  })
+  .catch((error) => {
+    throw error
+ });
+};
   
   export const getGenres = () => {
     return fetch(
