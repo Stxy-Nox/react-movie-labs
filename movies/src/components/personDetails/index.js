5d385b9cee17800d23ef152681aec66b083eb1f3
SytxNox <seven_night_w@hotmail.com>
2024-11-24 16:12:42 +0000

    add aka  role brithday chip

--- movies/src/components/personDetails/index.js
+++ movies/src/components/personDetails/index.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
-import StarRate from "@mui/icons-material/StarRate";
-import Fab from "@mui/material/Fab";
 import Typography from "@mui/material/Typography";
-import Drawer from "@mui/material/Drawer";
-import MovieReviews from "../movieReviews"
+import CakeIcon from '@mui/icons-material/Cake';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 
 
 const root = {
@@ -18,7 +17,43 @@ const root = {
 };
 const chip = { margin: 0.5 };
 
-const PersonDetails = ({ movie }) => {  
+const PersonDetails = ({ person }) => {  
+
+    return (
+        <>  
+        <Typography variant="h5" component="h3">
+            Biography
+        </Typography>
+        
+        <Typography variant="h6" component={"p"}>
+            {person.biography}
+        </Typography>
+            
+        <Paper component="ul" sx={{...root}}>
+            <li>
+                <Chip label="AKA" sx={{...chip}} color="secondry" />
+            </li>
+            {person.also_known_as.map((a) => (
+            <li key={a.name}>
+                <Chip label={a.name} sx={{...chip}} />
+            </li>
+            ))}
+        </Paper>
+
+        <Paper component={"ul"} sx={{...root}}>
+            <Chip icon={<AccountCircleIcon/>} label={person.known_for_department}/>
+            <Chip icon={<CakeIcon/>} label={person.birthday}/>
+
+        </Paper>
+        
+        
+        
+        
+        
+        
+        
+        </>
+    )
   
 };
 export default PersonDetails ;
