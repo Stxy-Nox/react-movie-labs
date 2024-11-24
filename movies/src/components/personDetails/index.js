7002bca2c4003ec52d1fe75c5d85b1111039aeb6
SytxNox <seven_night_w@hotmail.com>
2024-11-24 15:53:34 +0000

    import movie deatils to person detail

---
+++ movies/src/components/personDetails/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Chip from "@mui/material/Chip";
+import Paper from "@mui/material/Paper";
+import StarRate from "@mui/icons-material/StarRate";
+import Fab from "@mui/material/Fab";
+import Typography from "@mui/material/Typography";
+import Drawer from "@mui/material/Drawer";
+import MovieReviews from "../movieReviews"
+
+
+const root = {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    listStyle: "none",
+    padding: 1.5,
+    margin: 0,
+};
+const chip = { margin: 0.5 };
+
+const PersonDetails = ({ movie }) => {  
+  
+};
+export default PersonDetails ;
